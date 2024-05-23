52694bb04b88ffc664e25814187003647d9f6031
jooYeon <jooyeonstudyios@gmail.com>
2024-05-23 21:11:27 +0900

    first init

---
+++ DietLog-MVVM-SwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DietLog-MVVM-SwiftUI
+//
+//  Created by Jooyeon Kang on 5/23/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DietLog-MVVM-SwiftUI/DietLog_MVVM_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DietLog_MVVM_SwiftUIApp.swift
+//  DietLog-MVVM-SwiftUI
+//
+//  Created by Jooyeon Kang on 5/23/24.
+//
+
+import SwiftUI
+
+@main
+struct DietLog_MVVM_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
